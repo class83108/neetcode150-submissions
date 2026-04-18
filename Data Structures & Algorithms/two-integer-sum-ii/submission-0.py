87fe04ba23fb63afa14a52aef93ff046b43ef20b
Eason chuang <95297596+class83108@users.noreply.github.com>
2026-04-18 14:06:56 +0800

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        for idx, number in enumerate(numbers):
+            if number >= target:
+                break
+            value = target - number
+
+            for i in range(idx+1, len(numbers)):
+                if numbers[i] > value:
+                    break
+                if numbers[i] == value:
+                    return [idx+1, i+1]
+                    
+            
+
+        
+        return result
+
+        
