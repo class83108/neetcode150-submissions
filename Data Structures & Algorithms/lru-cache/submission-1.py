e2727a4866fbe5afb7b49f8bb3c8fd2e781ba106
Eason chuang <95297596+class83108@users.noreply.github.com>
2026-07-19 20:45:12 +0800

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,55 @@
+class DoubleLinkListed:
+    def __init__(self, key:int, value: int):
+        self.key = key
+        self.value = value
+        self.prev, self.next = None, None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.head = DoubleLinkListed(-1, -1)
+        self.tail = DoubleLinkListed(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+        self.hashMap = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.hashMap:
+            return -1
+        node = self.hashMap[key]
+        self.remove_node(node)
+        self.add_to_tail(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.hashMap:
+            new_node = DoubleLinkListed(key, value)
+            self.hashMap[key] = new_node
+            self.add_to_tail(new_node)
+
+            if len(self.hashMap) > self.capacity:
+                del self.hashMap[self.head.next.key]
+                self.remove_node(self.head.next)
+                
+        else:
+            node = self.hashMap[key]
+            node.value = value
+            self.remove_node(node)
+            self.add_to_tail(node)
+
+    
+    def remove_node(self, node: DoubleLinkListed) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def add_to_tail(self, node: DoubleLinkListed) -> None:
+        prev_node = self.tail.prev
+        node.prev = prev_node
+        node.next = self.tail
+        prev_node.next = node
+        self.tail.prev = node
+    
+    
+        
